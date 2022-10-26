a2a8208fd4fff2d54fb1c8da1cbd67774267c0f3
Drslx <drslxprogramador@gmail.com>
2022-10-25 21:21:46 -0300

    loading

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import "./App.css";
 
 import { useState, useEffect } from "react";
 
-// 4 - custom hook
+// 4 - Import Custom hook
 import { useFetch } from "./hooks/useFetch";
 
+// Url Api
 const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
 
-  // 4 - custom
-  const { data: items } = useFetch(url);
+  // 4 - Custom hooks
+  const { data: items, httpConfig, loading} = useFetch(url);
 
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
 
   // 1 - Get data
-  /*   useEffect(() => {
+  /*      useEffect(() => {
     async function fetchData() {
       const res = await fetch(url);
       const data = await res.json();
@@ -26,9 +27,9 @@ function App() {
     }
 
     fetchData();
-  }, []);
- */
-  // 2 - Submit data
+  }, []); */
+
+  // 2 - Add products
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -37,14 +38,19 @@ function App() {
       price,
     };
 
-    const res = await fetch(url, {
+    // Codigo antigo para fazer Post
+    /*  const res = await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(product),
     });
+
     // 3 - Loading dynamic
     const addedProduct = await res.json();
     setProducts((prevProducts) => [...prevProducts, addedProduct]);
+*/
+    // 5 - Refactoring POST
+    httpConfig(product, "POST");
 
     setName("");
     setPrice("");
@@ -54,12 +60,15 @@ function App() {
     <div className="App">
       <h1>Lista de Produtos</h1>
 
+      {/* 6 - loading */}
+      {loading && <p>Carregando...</p>}
       <ul>
-        {items && items.map((product) => (
-          <li key={product.id}>
-            {product.name} - {product.price}
-          </li>
-        ))}
+        {items &&
+          items.map((product) => (
+            <li key={product.id}>
+              {product.name} - {product.price}
+            </li>
+          ))}
       </ul>
 
       <div className="add-product">
